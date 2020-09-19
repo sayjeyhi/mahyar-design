e041027f3a53746ee851d409baf0581697844fe2
Loke <loke@crossroads.se>
2020-09-19 16:18:30 +0200

    refactor: replace contact icons with social media

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -42,18 +42,18 @@ const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) =>
     </Content>
     <Divider speed={0.1} offset={offset} factor={factor}>
       <UpDown>
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="70%" top="20%" />
-        <SVG icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
+        <SVG icon="gmail" hiddenMobile width={8} color="icon_brightest" left="70%" top="20%" />
+        <SVG icon="gitlab" width={8} stroke color="icon_darkest" left="25%" top="5%" />
       </UpDown>
       <UpDownWide>
-        <SVG icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
-        <SVG icon="circle" width={6} color="icon_brightest" left="85%" top="15%" />
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+        <SVG icon="twitter" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+        <SVG icon="facebook" width={8} color="icon_darkest" left="85%" top="15%" />
+        <SVG icon="slack" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
       </UpDownWide>
-      <SVG icon="circle" width={6} color="icon_brightest" left="4%" top="20%" />
-      <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
-      <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
-      <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
+      <SVG icon="github" width={12} color="icon_brightest" left="4%" top="60%" />
+      <SVG icon="linkedin" width={6} color="icon_brightest" left="4%" top="20%" />
+      <SVG icon="messenger" width={12} color="icon_darkest" left="70%" top="60%" />
+      <SVG icon="instagram" width={12} color="icon_darkest" left="20%" top="30%" />
     </Divider>
   </div>
 )
