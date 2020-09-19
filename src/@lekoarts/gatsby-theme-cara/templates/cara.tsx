b0f55e488760804f4ec4047f924840f3a3506001
Loke <loke@crossroads.se>
2020-09-19 16:17:56 +0200

    feat: add blog section

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -3,16 +3,18 @@ import { Parallax } from "react-spring/renderprops-addons.cjs"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Projects from "../components/projects"
+import Blog from "../components/Blog"
 import About from "../components/about"
 import Contact from "../components/contact"
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={5}>
+    <Parallax pages={7}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
-      <About offset={3} factor={1} />
-      <Contact offset={4} factor={1} />
+      <Blog offset={3} factor={2} />
+      <About offset={5} factor={1} />
+      <Contact offset={6} factor={1} />
     </Parallax>
   </Layout>
 )
