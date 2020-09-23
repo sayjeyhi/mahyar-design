fab6e615c3a48908269f4d1001e68152220e18a0
Loke Carlsson <carlssonloke@gmail.com>
2020-09-23 20:15:10 +0200

    fix: adjust svgs

--- src/@lekoarts/gatsby-theme-cara/components/about.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about.tsx
@@ -31,9 +31,9 @@ const About = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
         <SVG icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
       </UpDownWide>
       <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="4%" top="20%" />
-      <SVG icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
+      <SVG icon="circle" width={12} color="icon_darkest" left="10%" top="60%" />
       <SVG icon="box" width={6} color="icon_orange" left="10%" top="10%" />
-      <SVG icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
+      <SVG icon="box" width={12} color="icon_darkest" left="20%" top="22%" />
       <SVG icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
     </Divider>
     <Content speed={0.1} offset={offset} factor={factor}>
