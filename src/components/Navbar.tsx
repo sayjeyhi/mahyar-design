c5fde732e43758e2a2bb89c02a83d00d77bc326a
Loke Carlsson <carlssonloke@gmail.com>
2021-08-22 17:19:03 +0200

    feat: intial page structure and layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,132 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import Link from 'next/link';
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 
-import { Section } from '../layouts/Section';
-import { Logo } from '../templates/Logo';
+const Navbar = () => {
+  const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+  ];
 
-const Navbar = () => (
-  <Section className="py-6 w-full">
-    <div className="flex flex-wrap justify-between items-center">
-      <div>
-        <Link href="/">
-          <a>
-            <Logo xl />
-          </a>
-        </Link>
+  return (
+    <Popover as="header" className="relative">
+      <div className="bg-gray-900 pt-6">
+        <nav
+          className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+          aria-label="Global"
+        >
+          <div className="flex items-center flex-1">
+            <div className="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-8 w-auto sm:h-10"
+                  src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
+                  alt=""
+                />
+              </a>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="hidden space-x-8 md:flex md:ml-10">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-base font-medium text-white hover:text-gray-300"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="hidden md:flex md:items-center md:space-x-6">
+            <a
+              href="#"
+              className="text-base font-medium text-white hover:text-gray-300"
+            >
+              Log in
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+            >
+              Start free trial
+            </a>
+          </div>
+        </nav>
       </div>
 
-      <nav>
-        <ul className="navbar flex items-center font-medium text-xl text-gray-800">
-          <li>
-            <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-              <a>GitHub</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Sign in</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <style jsx>
-        {`
-          .navbar :global(li:not(:first-child)) {
-            @apply mt-0;
-          }
-
-          .navbar :global(li:not(:last-child)) {
-            @apply mr-5;
-          }
-        `}
-      </style>
-    </div>
-  </Section>
-);
+      <Transition
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+        >
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+                  alt=""
+                />
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                  <span className="sr-only">Close menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="pt-5 pb-6">
+              <div className="px-2 space-y-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="mt-6 px-5">
+                <a
+                  href="#"
+                  className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                >
+                  Start free trial
+                </a>
+              </div>
+              <div className="mt-6 px-5">
+                <p className="text-center text-base font-medium text-gray-500">
+                  Existing customer?{' '}
+                  <a href="#" className="text-gray-900 hover:underline">
+                    Login
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
+};
 
 export { Navbar };
--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import Footer from '../components/Footer';
 import { Navbar } from '../components/Navbar';
-import { Footer } from '../templates/Footer';
 import { AppConfig } from '../utils/AppConfig';
 import { Meta } from './Meta';
 
 const Default = ({ children }: any) => (
-  <div className="antialiased text-gray-600 flex flex-col h-full">
+  <div className="antialiased text-gray-600 bg-white flex flex-col h-full">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Navbar />
-    <div className="flex-layout">{children}</div>
+    <main className="flex-layout">{children}</main>
     <Footer />
   </div>
 );
