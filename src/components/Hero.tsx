4d43d6231f065d1f9825883611c618caeebe0c09
Loke Carlsson <carlssonloke@gmail.com>
2021-08-22 20:15:40 +0200

    feat: rework hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,30 @@
-import { ReactNode } from 'react';
+import React from 'react';
 
-type IHeroOneButtonProps = {
-  title: ReactNode;
-  description: string;
-  button: ReactNode;
-};
-
-const HeroOneButton = (props: IHeroOneButtonProps) => (
-  <header className="text-center">
-    <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
-      {props.title}
-    </h1>
-    <div className="text-2xl mt-4 mb-16">{props.description}</div>
-
-    {props.button}
-  </header>
+const Hero = () => (
+  <div className="flex pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden items-center lg:min-h-[800px]">
+    <div className="mx-auto max-w-7xl lg:px-8">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+        <div className="max-w-md px-4 mx-auto sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+          <div className="lg:py-24">
+            <h1 className="pb-3 mt-4 text-4xl font-extrabold tracking-tight text-white sm:pb-5 sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+              <span className="block">Building the web</span>
+              <span className="block text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-cyan-400">
+                one line of code
+              </span>
+              <span className="block"> at a time.</span>
+            </h1>
+          </div>
+        </div>
+        <div className="mt-12 -mb-16 lg:ml-32 sm:-mb-48 lg:m-0 lg:relative">
+          <img
+            className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+            src="/assets/images/code.svg"
+            alt="code"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
-export { HeroOneButton };
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,9 @@ import {
   ServerIcon,
   ShieldCheckIcon,
 } from '@heroicons/react/outline';
-import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid';
+import { ExternalLinkIcon } from '@heroicons/react/solid';
+
+import Hero from '../components/Hero';
 
 const features = [
   {
@@ -111,92 +113,19 @@ const blogPosts = [
 const Index = () => {
   return (
     <div>
-      <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
-        <div className="mx-auto max-w-7xl lg:px-8">
-          <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-              <div className="lg:py-24">
-                <a
-                  href="#"
-                  className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
-                >
-                  <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">
-                    We&apos;re hiring
-                  </span>
-                  <span className="ml-4 text-sm">Visit our careers page</span>
-                  <ChevronRightIcon
-                    className="ml-2 w-5 h-5 text-gray-500"
-                    aria-hidden="true"
-                  />
-                </a>
-                <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                  <span className="block">A better way to</span>
-                  <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
-                    ship web apps
-                  </span>
-                </h1>
-                <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat.
-                </p>
-                <div className="mt-10 sm:mt-12">
-                  <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
-                    <div className="sm:flex">
-                      <div className="min-w-0 flex-1">
-                        <input
-                          id="email"
-                          type="email"
-                          placeholder="Enter your email"
-                          className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                        />
-                      </div>
-                      <div className="mt-3 sm:mt-0 sm:ml-3">
-                        <button
-                          type="submit"
-                          className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                        >
-                          Start free trial
-                        </button>
-                      </div>
-                    </div>
-                    <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                      Start your free 14-day trial, no credit card necessary. By
-                      providing your email, you agree to our{' '}
-                      <a href="#" className="font-medium text-white">
-                        terms or service
-                      </a>
-                      .
-                    </p>
-                  </form>
-                </div>
-              </div>
-            </div>
-            <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
-                <img
-                  className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                  src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
 
       {/* Feature section with screenshot */}
-      <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
-        <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+      <div className="relative pt-16 bg-gray-50 sm:pt-24 lg:pt-32">
+        <div className="max-w-md px-4 mx-auto text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
           <div>
-            <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+            <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">
               Serverless
             </h2>
-            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+            <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               No server? No problem.
             </p>
-            <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+            <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
               Phasellus lorem quam molestie id quisque diam aenean nulla in.
               Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
               condimentum id viverra nulla.
@@ -213,15 +142,15 @@ const Index = () => {
       </div>
 
       {/* Feature section with grid */}
-      <div className="relative bg-white py-16 sm:py-24 lg:py-32">
-        <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-          <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+      <div className="relative py-16 bg-white sm:py-24 lg:py-32">
+        <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+          <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">
             Deploy faster
           </h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Everything you need to deploy your app
           </p>
-          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
             Phasellus lorem quam molestie id quisque diam aenean nulla in.
             Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
             condimentum id viverra nulla.
@@ -230,17 +159,17 @@ const Index = () => {
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
               {features.map((feature) => (
                 <div key={feature.name} className="pt-6">
-                  <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="flow-root px-6 pb-8 rounded-lg bg-gray-50">
                     <div className="-mt-6">
                       <div>
-                        <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                        <span className="inline-flex items-center justify-center p-3 rounded-md shadow-lg bg-gradient-to-r from-teal-500 to-cyan-600">
                           <feature.icon
-                            className="h-6 w-6 text-white"
+                            className="w-6 h-6 text-white"
                             aria-hidden="true"
                           />
                         </span>
                       </div>
-                      <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                      <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
                         {feature.name}
                       </h3>
                       <p className="mt-5 text-base text-gray-500">
@@ -261,10 +190,10 @@ const Index = () => {
           <div className="relative lg:-my-8">
             <div
               aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
+              className="absolute inset-x-0 top-0 bg-white h-1/2 lg:hidden"
             />
-            <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-              <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+            <div className="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
+              <div className="overflow-hidden shadow-xl aspect-w-10 aspect-h-6 rounded-xl sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
                 <img
                   className="object-cover lg:h-full lg:w-full"
                   src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
@@ -274,11 +203,11 @@ const Index = () => {
             </div>
           </div>
           <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
-            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+            <div className="max-w-md px-4 mx-auto sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
               <blockquote>
                 <div>
                   <svg
-                    className="h-12 w-12 text-white opacity-25"
+                    className="w-12 h-12 text-white opacity-25"
                     fill="currentColor"
                     viewBox="0 0 32 32"
                     aria-hidden="true"
@@ -306,35 +235,35 @@ const Index = () => {
       </div>
 
       {/* Blog section */}
-      <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
+      <div className="relative py-16 bg-gray-50 sm:py-24 lg:py-32">
         <div className="relative">
-          <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-            <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+          <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">
               Learn
             </h2>
-            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+            <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               Helpful Resources
             </p>
-            <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
+            <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
               Phasellus lorem quam molestie id quisque diam aenean nulla in.
               Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
               condimentum id viverra nulla.
             </p>
           </div>
-          <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
+          <div className="grid max-w-md gap-8 px-4 mx-auto mt-12 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
             {blogPosts.map((post) => (
               <div
                 key={post.id}
-                className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+                className="flex flex-col overflow-hidden rounded-lg shadow-lg"
               >
                 <div className="flex-shrink-0">
                   <img
-                    className="h-48 w-full object-cover"
+                    className="object-cover w-full h-48"
                     src={post.imageUrl}
                     alt=""
                   />
                 </div>
-                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex flex-col justify-between flex-1 p-6 bg-white">
                   <div className="flex-1">
                     <p className="text-sm font-medium text-cyan-600">
                       <a href={post.category.href} className="hover:underline">
@@ -350,11 +279,11 @@ const Index = () => {
                       </p>
                     </a>
                   </div>
-                  <div className="mt-6 flex items-center">
+                  <div className="flex items-center mt-6">
                     <div className="flex-shrink-0">
                       <a href={post.author.href}>
                         <img
-                          className="h-10 w-10 rounded-full"
+                          className="w-10 h-10 rounded-full"
                           src={post.author.imageUrl}
                           alt={post.author.name}
                         />
@@ -384,7 +313,7 @@ const Index = () => {
       <div className="relative bg-gray-900">
         <div className="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
           <img
-            className="w-full h-full object-cover"
+            className="object-cover w-full h-full"
             src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
             alt=""
           />
@@ -393,12 +322,12 @@ const Index = () => {
             className="absolute inset-0 bg-gradient-to-r from-teal-500 to-cyan-600 mix-blend-multiply"
           />
         </div>
-        <div className="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
+        <div className="relative max-w-md px-4 py-12 mx-auto sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
           <div className="md:ml-auto md:w-1/2 md:pl-10">
-            <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">
+            <h2 className="text-base font-semibold tracking-wider text-gray-300 uppercase">
               Award winning support
             </h2>
-            <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
+            <p className="mt-2 text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
               We’re here to help
             </p>
             <p className="mt-3 text-lg text-gray-300">
@@ -411,11 +340,11 @@ const Index = () => {
               <div className="inline-flex rounded-md shadow">
                 <a
                   href="#"
-                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
+                  className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-gray-900 bg-white border border-transparent rounded-md hover:bg-gray-50"
                 >
                   Visit the help center
                   <ExternalLinkIcon
-                    className="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                    className="w-5 h-5 ml-3 -mr-1 text-gray-400"
                     aria-hidden="true"
                   />
                 </a>
