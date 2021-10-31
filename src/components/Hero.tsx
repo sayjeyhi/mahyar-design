9b753803bbddcb1d10d51072e7f3391dc6b35542
Loke <loke@crossroads.se>
2021-10-31 20:55:49 +0100

    feat: rework hero size and positioning

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Hero = () => (
-  <div className="w-full h-full overflow-auto bg-cover bg-hero">
-    <div className="flex w-full h-full items-center lg:min-h-[1000px] mb-24">
+  <div className="w-full h-full overflow-auto bg-bottom bg-cover bg-hero">
+    <div className="flex w-full h-full items-center lg:min-h-[90vh]">
       <div className="mx-auto max-w-7xl">
         <h1 className="pb-3 mt-4 mb-40 text-4xl font-extrabold leading-[6rem] tracking-tight text-white sm:pb-5 sm:mt-5 sm:text-6xl lg:mt-6 xl:text-7xl">
           <span className="block">Building the web</span>
