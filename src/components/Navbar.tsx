f370ea4c91af29dff95578e609b5c4e5ef74b7ac
Loke <loke@crossroads.se>
2021-11-07 22:34:48 +0100

    refactor: add base styles and adjust nav styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,31 +27,31 @@ const MobileMenu = () => (
   >
     <Popover.Panel
       focus
-      className="p-2 transform origin-top inset-x-0 transition top-0 absolute md:hidden"
+      className="absolute inset-x-0 top-0 p-2 transition origin-top transform md:hidden"
     >
-      <div className="bg-white rounded-lg shadow-md ring-black ring-1 ring-opacity-5 overflow-hidden">
-        <div className="flex px-5 pt-4 items-center justify-between">
+      <div className="overflow-hidden bg-white rounded-lg shadow-md ring-black ring-1 ring-opacity-5">
+        <div className="flex items-center justify-between px-5 pt-4">
           <div>
             <img
-              className="h-8 w-auto"
+              className="w-auto h-8"
               src="/assets/images/Icon-text-black.svg"
               alt="logo"
             />
           </div>
           <div className="-mr-2">
-            <Popover.Button className="bg-white rounded-md p-2 text-gray-400 inline-flex items-center justify-center hover:bg-gray-100 focus:outline-none focus:ring-inset focus:ring-2 focus:ring-cyan-600">
+            <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-inset focus:ring-2 focus:ring-cyan-600">
               <span className="sr-only">Close menu</span>
-              <XIcon className="h-6 w-6" aria-hidden="true" />
+              <XIcon className="w-6 h-6" aria-hidden="true" />
             </Popover.Button>
           </div>
         </div>
         <div className="pt-5 pb-6">
-          <div className="space-y-1 px-2">
+          <div className="px-2 space-y-1">
             {navigation.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
-                className="rounded-md font-medium text-base py-2 px-3 text-gray-900 block hover:bg-gray-50"
+                className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50"
               >
                 {item.name}
               </a>
@@ -77,19 +77,19 @@ const Navbar = () => {
         "fixed z-50 w-full py-4 transition-colors duration-500 bg-white",
         {
           "bg-transparent": hideBG && onIndex,
-        }
+        },
       )}
     >
       <div
-        className="flex mx-auto max-w-7xl px-4 relative items-center justify-between sm:px-6"
+        className="relative flex items-center justify-between px-4 mx-auto max-w-7xl sm:px-6"
         aria-label="Global"
       >
-        <div className="flex flex-1 items-center justify-between">
-          <div className="flex w-full items-center justify-between md:w-auto">
-            <Link to="/" className="text-primary-yellow py-2 px-4">
+        <div className="flex items-center justify-between flex-1">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <Link to="/" className="px-4 py-2 text-primary-yellow">
               <span className="sr-only">Workflow</span>
               <img
-                className="h-8 text-primary-red w-auto sm:h-10"
+                className="w-auto h-8 text-primary-red sm:h-10"
                 src={
                   hideBG && onIndex
                     ? "/assets/images/Icon-white.svg"
@@ -98,27 +98,27 @@ const Navbar = () => {
                 alt="Logo"
               />
             </Link>
-            <div className="flex -mr-2 items-center md:hidden">
-              <Popover.Button className="bg-background rounded-md p-2 text-gray-400 inline-flex items-center justify-center focus-ring-inset focus:outline-none focus:ring-white focus:ring-2">
+            <div className="flex items-center -mr-2 md:hidden">
+              <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md bg-background focus-ring-inset focus:outline-none focus:ring-white focus:ring-2">
                 <span className="sr-only">Open main menu</span>
-                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                <MenuIcon className="w-6 h-6" aria-hidden="true" />
               </Popover.Button>
             </div>
           </div>
-          <div className="space-x-8 hidden md:flex md:ml-10">
+          <div className="hidden space-x-8 md:flex md:ml-10">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
                 className={cn(
-                  "px-4 py-2 text-base font-medium hover:underline transition-colors duration-500",
+                  "px-4 py-2 font-bold text-xl no-underline hover:underline transition-colors duration-500",
                   {
                     "text-white": hideBG && onIndex,
-                  }
+                  },
                 )}
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
         </div>
