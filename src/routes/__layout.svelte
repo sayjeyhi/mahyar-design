23abcbe12ac4fe54f577f3c0a088977711ce19e0
Loke <loke@crossroads.se>
2021-11-13 14:02:27 +0100

    feat: add transitions to default layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,26 @@
+<script lang="ts" context="module">
+  export const load = async ({ page }) => ({
+    props: {
+      key: page.path,
+    },
+  });
+</script>
+
 <script lang="ts">
   import Header from "$components/Header.svelte";
   import Footer from "$components/Footer.svelte";
+  import PageTransition from "$components/PageTransition.svelte";
   import "../app.css";
+
+  export let key: string;
 </script>
 
 <Header />
 
 <main class="flex flex-1 flex-col p-4 w-full max-w-5xl m-auto box-border mt-6 mb-20">
-  <slot />
+  <PageTransition refresh={key}>
+    <slot />
+  </PageTransition>
 </main>
 
 <Footer />
