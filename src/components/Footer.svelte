a370b35bbb441547e422b1fce719422fea9ddc2d
Loke <loke@crossroads.se>
2021-11-14 20:05:04 +0100

    feat: adjust padding

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="flex flex-col justify-center items-center p-10 sm:px-0 bg-background--faded">
+<footer class="flex flex-col justify-center items-center p-8 sm:px-0 bg-background--faded">
   <p>
     All rights reserved © Loke Carlsson {new Date().getFullYear()}
   </p>
