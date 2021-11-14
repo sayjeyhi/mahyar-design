f0f42ac5f337dac2f02b59e1890aa8cd9adc3abd
Loke <loke@crossroads.se>
2021-11-14 18:07:08 +0100

    chore: add support for postcss variables and nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,16 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const tailwindcss = require("tailwindcss")
+const autoprefixer = require("autoprefixer")
+const cssnano = require("cssnano")
+const nested = require("postcss-nested")
+const variables = require("postcss-simple-vars")
 
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const mode = process.env.NODE_ENV
+const dev = mode === "development"
 
 const config = {
   plugins: [
+    nested(),
+    variables(),
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(),
     //But others, like autoprefixer, need to run after,
@@ -16,6 +20,6 @@ const config = {
         preset: "default",
       }),
   ],
-};
+}
 
-module.exports = config;
+module.exports = config
