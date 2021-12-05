581b202ade24ffd1f4d44c233bbfc7a779319a29
Loke <loke@crossroads.se>
2021-12-05 20:40:47 +0100

    feat: rework jounal posts

--- src/routes/journal.svelte
+++ src/routes/journal.svelte
@@ -31,13 +31,29 @@
 </svelte:head>
 
 <div class="content">
-  <h1 class="mb-6">Journal</h1>
+  <h1 class="mb-6">Let's learn together!</h1>
 
   <ul>
-    {#each posts as { slug, title, author, description, date }}
-      <li>
-        <a href={`/journal/${slug}`}>{title}</a>
-      </li>
-    {/each}
+    <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+      {#each posts as { slug, title, description, date }}
+        <div>
+          <a href={`/journal/${slug}`} class="mt-2 block">
+            <p class="text-sm text-gray-500 mb-1">
+              <time dateTime={date}>{new Date(date).toLocaleDateString("sv-SE")}</time>
+            </p>
+            <p class="text-xl font-semibold text-gray-100">{title}</p>
+            <p class="mt-3 text-base text-gray-500">{description}</p>
+          </a>
+          <div class="mt-3">
+            <a
+              href={`/journal/${slug}`}
+              class="text-base font-semibold text-primary hover:underline"
+            >
+              Read full story
+            </a>
+          </div>
+        </div>
+      {/each}
+    </div>
   </ul>
 </div>
