d7828e5c5f1537b4d03bb600fa0ece96af51c257
Loke <loke@crossroads.se>
2021-12-05 22:30:41 +0100

    feat: add hover on journal posts

--- src/routes/journal.svelte
+++ src/routes/journal.svelte
@@ -37,13 +37,13 @@
     <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-12">
       {#each posts as { slug, title, description, date }}
         <div>
-          <a href={`/journal/posts/${slug}`} class="mt-2 block">
+          <a href={`/journal/posts/${slug}`} class="mt-2 block group">
             <p class="text-sm text-gray-500 mb-1">
               <time dateTime={date}>{new Date(date).toLocaleDateString("sv-SE")}</time>
             </p>
-            <p class="text-xl font-semibold text-gray-100">{title}</p>
+            <p class="text-xl font-semibold text-gray-100 group-hover:underline">{title}</p>
             <p class="mt-3 text-base text-gray-500">{description}</p>
-            <p class="mt-3">Read full story</p>
+            <p class="mt-3 group-hover:underline">Read full story</p>
           </a>
         </div>
       {/each}
