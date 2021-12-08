02ea55e3de96c8db221b1fa70d219e24433c0cba
Loke <loke@crossroads.se>
2021-12-08 23:15:09 +0100

    feat: remove page transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,27 +1,14 @@
-<script lang="ts" context="module">
-  export const load = async ({ page }) => ({
-    props: {
-      key: page.path,
-    },
-  })
-</script>
-
 <script lang="ts">
   import Header from "$components/Header.svelte"
   import Footer from "$components/Footer.svelte"
-  import PageTransition from "$components/PageTransition.svelte"
 
   import "$styles/app.css"
-
-  export let key: string
 </script>
 
 <Header />
 
 <main class="flex flex-1 flex-col p-8 w-full m-auto box-border mt-6 mb-20 max-w-[900px]">
-  <PageTransition refresh={key}>
-    <slot />
-  </PageTransition>
+  <slot />
 </main>
 
 <Footer />
