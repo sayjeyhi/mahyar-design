75630019e2750e33fde3f0cb0be52f4d288d8782
Loke <loke@crossroads.se>
2021-12-08 23:15:00 +0100

    chore: rename id

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -19,18 +19,11 @@
     aria-hidden="true"
   >
     <defs>
-      <pattern
-        id="85737c0e-0916-41d7-917f-596dc7edfa27"
-        x="0"
-        y="0"
-        width="20"
-        height="20"
-        patternUnits="userSpaceOnUse"
-      >
+      <pattern id="pattern" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
         <rect x="0" y="0" width="4" height="4" class="text-faded" fill="currentColor" />
       </pattern>
     </defs>
-    <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+    <rect width="404" height="404" fill="url(#pattern)" />
   </svg>
   <svg
     class="absolute right-full bottom-0 transform translate-x-full -translate-y-1/2"
@@ -41,18 +34,11 @@
     aria-hidden="true"
   >
     <defs>
-      <pattern
-        id="85737c0e-0916-41d7-917f-596dc7edfa27"
-        x="0"
-        y="0"
-        width="20"
-        height="20"
-        patternUnits="userSpaceOnUse"
-      >
+      <pattern id="pattern" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
         <rect x="0" y="0" width="4" height="4" class="text-faded" fill="currentColor" />
       </pattern>
     </defs>
-    <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+    <rect width="404" height="404" fill="url(#pattern)" />
   </svg>
 </div>
 
