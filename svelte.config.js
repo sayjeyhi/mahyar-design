02e63a15b9aeec0a7161ac17a517aa764ef1a353
Loke Carlsson <hi@loke.dev>
2022-06-20 09:52:18 +0000

    fix: configure vite server HMR to work with gitpod

--- svelte.config.js
+++ svelte.config.js
@@ -24,6 +24,18 @@ const config = {
     },
     adapter: netlify(),
     vite: {
+      server: {
+        hmr: process.env.GITPOD_WORKSPACE_URL
+          ? {
+              // Due to port fowarding, we have to replace
+              // 'https' with the forwarded port, as this
+              // is the URI created by Gitpod.
+              host: process.env.GITPOD_WORKSPACE_URL.replace("https://", "3000-"),
+              protocol: "wss",
+              clientPort: 443
+            }
+          : true
+      },
       test: {
         environment: "jsdom",
         coverage: {
