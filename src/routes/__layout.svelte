064b49b131b566aa33bb7bff0070136659b0581f
Loke <loke.carlsson@fyndiq.com>
2022-09-24 11:57:32 +0200

    feat: add metadata

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,6 +17,14 @@
   })
 </script>
 
+<svelte:head>
+  <meta name="og:title" content="Loke.dev" />
+  <meta
+    name="og:description"
+    content="Loke is a frontend developer based in Lund, Sweden. Read more about me or what services I can help you with."
+  />
+</svelte:head>
+
 <Header />
 
 <main class="flex flex-1 flex-col p-8 w-full m-auto box-border mt-6 mb-20 max-w-[900px]">
